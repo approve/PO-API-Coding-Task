1ecf9f828d2d785b07cb7214f18ab679a780dc81
MichaMilsTip <michael.milstein@tipalti.com>
2025-09-02 21:07:04 +0300

    add route not found endpoint

--- server/index.js
+++ server/index.js
@@ -11,6 +11,13 @@ app.get('/api/health', (req, res) => {
   res.json({ message: 'Server is running!' });
 });
 
+// implement your solution here
+
+
+app.use((req, res, next) => {
+  res.status(404).json({ error: 'Sorry, the requested endpoint was not found.' });
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
